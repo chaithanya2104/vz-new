de0e833179fe3ad9b740a6dd499d0dade9b7b206
chaithanya2104 <kandulachaithanya21@gmail.com>
2019-12-31 12:59:38 -0600

    Hystrix

--- Item/src/main/java/com/example/Item/controller/ItemController.java
+++ Item/src/main/java/com/example/Item/controller/ItemController.java
@@ -2,6 +2,8 @@ package com.example.Item.controller;
 
 import com.example.Item.model.Items;
 import com.example.Item.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,4 +38,16 @@ public class ItemController {
 //    public Double getPriceByName(@PathVariable String name){
 //            return getItemByName(name).getPrice();
 //    }
+
+    @GetMapping("/items/hystrix-fault-tolerance")
+    @HystrixCommand(fallbackMethod = "faultToleranceFallbackMethod")
+    public Items testFaultTolerance(){
+        throw  new RuntimeException("end point currently down...");
+    }
+
+    public Items faultToleranceFallbackMethod(){
+        Items dummyObject = new Items("dummy name", 99.99, "dummydescription");
+        return dummyObject;
+    }
+
 }
